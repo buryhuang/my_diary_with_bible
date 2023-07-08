4600ef9bc54187db1321acf48a0a25c439ee919a
Yiyang Li <yiyangli2010@gmail.com>
2023-07-08 11:09:20 -0700

    add js-cookie; read username from cookie; if missing, render a form and save username to cookie from the form

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,29 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Chat, { Bubble, useMessages } from '@chatui/core';
+import Cookies from 'js-cookie';
+import Chat, { Bubble, useMessages, Input, Button, Text } from '@chatui/core';
 import '@chatui/core/dist/index.css';
 
+const Form = ({
+  saveUsername
+}) => {
+  const [username, setUsername] = useState('');
+  return (
+    <div>
+      <h3>Welcome to the Bible Verse Chat!</h3>
+      <Input maxLength={20} value={username} onChange={val => setUsername(val)} placeholder="Please type your user name..." />
+      <Text breakWord>This is a hackathon project, username is stored in the cookies, please set a secret name and keep it private. </Text>
+      <Button color="primary" onClick={() => {
+        Cookies.set('username', username)
+        saveUsername(username)
+      }}>Save</Button>
+    </div>
+  );
+}
+
 const App = () => {
+  const [username, setUsername] = useState(Cookies.get('username'));
+  const hasUsername = !!username
   const { messages, appendMsg, setTyping } = useMessages([]);
 
   async function handleSend(type, val) {
@@ -17,7 +37,7 @@ const App = () => {
       setTyping(true);
 
       const response = await axios.post('http://127.0.0.1:5000/chat', {
-        user_id: 'buryrocks',
+        user_id: username,
         query: val,
       });
 
@@ -37,12 +57,14 @@ const App = () => {
   }
 
   return (
-      <Chat
-          navbar={{ title: 'Assistant' }}
-          messages={messages}
-          renderMessageContent={renderMessageContent}
-          onSend={handleSend}
-      />
+    hasUsername ? 
+    <Chat
+      navbar={{ title: 'Assistant' }}
+      messages={messages}
+      renderMessageContent={renderMessageContent}
+      onSend={handleSend}
+    /> : 
+    <Form saveUsername={setUsername}/>
   );
 };
 
